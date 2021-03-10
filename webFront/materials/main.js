3ddad2a9c6d36595f951b4cbd62826b32192f504
MetereauFrederic <frederic.metereau@etu.univ-cotedazur.fr>
2021-03-10 16:31:02 +0100

    Exercice 1 et 2

--- webFront/materials/main.js
+++ webFront/materials/main.js
@@ -1,1 +1,13 @@
-document.getElementById("workingTest").innerHTML="It works";
+function retrieveAllContracts() {
+    var url = "https://api.jcdecaux.com/vls/v3/contracts?apiKey=" + document.getElementById("key").value;
+    var caller = new XMLHttpRequest();
+    caller.open("GET", url, true);
+    caller.setRequestHeader("Accept", "application/json");
+    caller.onload = contractsRetrieved;
+    caller.send();
+}
+
+function contractsRetrieved() {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+}
