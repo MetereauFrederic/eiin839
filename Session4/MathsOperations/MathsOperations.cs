ed7d9398de90a13e29f4c9b80c1afc123f77be18
MetereauFrederic <frederic.metereau@etu.univ-cotedazur.fr>
2021-03-24 14:55:30 +0100

    session 4

---
+++ Session4/MathsOperations/MathsOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsOperations
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class MathsOperations : IMathsOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public float Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Session4/SOAPWebServices/Program.cs
@@ -0,0 +1,15 @@
+﻿using ServiceReference1;
+using System;
+
+namespace SOAPWebServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperationsClient client = new MathsOperationsClient(MathsOperationsClient.EndpointConfiguration.BasicHttpBinding_IMathsOperations);
+            Console.WriteLine(client.AddAsync(4, 5).Result);
+            client.CloseAsync();
+        }
+    }
+}
